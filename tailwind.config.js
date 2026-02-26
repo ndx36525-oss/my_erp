34e594c9a3dc8b4f9324209dd0e98ebb8248b2c7
ndx36525-oss <ndx36525@gmail.com>
2026-02-25 20:35:40 -0500

    Confirmed login screen working locally

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      // You can add custom brand colors here later if you want
+    },
+  },
+  plugins: [],
+}
